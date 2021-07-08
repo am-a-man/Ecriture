27bc17ca4e6617e1a1e79179b3d96a8359e38b69
Aman Kumar <avy0219@gmail.com>
2021-07-08 20:42:33 +0530

    updating writing mechanism for parallel scripts

--- server/app/common/write.py
+++ server/app/common/write.py
@@ -1,9 +1,12 @@
 
 import json
+import threading
 class fileWrite:
     def __init__(self):
         with open("./api_outlet/content.json", "r",encoding="utf-8") as jsonData:
             self.data = json.load(jsonData)
+
+        self.threadingLock = threading.Lock()
       
     def write(self, dataFile):
         for i in dataFile.keys():
@@ -11,5 +14,7 @@ class fileWrite:
                 print("WARNING: this index already exists in the data, ignore if updating the present data")
             self.data[i] = dataFile[i]
         jsonData = json.dumps(self.data, indent=4)
+        self.threadingLock.acquire()
         with open("./api_outlet/content.json", "w", encoding="utf-8") as jsonDataFile:
             jsonDataFile.write(jsonData)
+        self.threadingLock.release()
