908feeb09e6bc82b9e8b98ea5a282a9c72cfc837
Aman Kumar <avy0219@gmail.com>
2021-07-09 21:21:22 +0530

    changing from multithreading to multiprocessing

--- server/app/common/write.py
+++ server/app/common/write.py
@@ -1,12 +1,13 @@
 
 import json
-import threading
+from multiprocessing import Lock
+
 class fileWrite:
     def __init__(self):
         with open("./api_outlet/content.json", "r",encoding="utf-8") as jsonData:
             self.data = json.load(jsonData)
 
-        self.threadingLock = threading.Lock()
+        self.processLock = Lock()
       
     def write(self, dataFile):
         for i in dataFile.keys():
@@ -14,7 +15,7 @@ class fileWrite:
                 print("WARNING: this index already exists in the data, ignore if updating the present data")
             self.data[i] = dataFile[i]
         jsonData = json.dumps(self.data, indent=4)
-        self.threadingLock.acquire()
+        self.processLock.acquire()
         with open("./api_outlet/content.json", "w", encoding="utf-8") as jsonDataFile:
             jsonDataFile.write(jsonData)
-        self.threadingLock.release()
+        self.processLock.release()
